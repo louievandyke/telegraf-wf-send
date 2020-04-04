ebe08e64b351bd8050f2c5af45c48918ab3cb1b1
sgunn <steven_gunn@intuit.com>
2020-04-03 22:49:55 -0700

    working all metrics

--- telegraf2wavefront.py
+++ telegraf2wavefront.py
@@ -14,8 +14,8 @@ logger = Log(__file__).logger()
 class Telegraf2Wavefront(object):
 
   def __init__(self):
-    self.filePath = 'metrics.out'
-    self.s3Bucket = 'intu-oim-prd-5-us-west-2-debug'
+    self.filePath = '/tmp/metrics.out'
+    self.s3Bucket = 'intu-oim-prd-ihp-01-us-west-2'
     self.s3Client = boto3.client("s3", config=Config(signature_version=UNSIGNED))
 
   def readFile(self):
@@ -23,22 +23,20 @@ class Telegraf2Wavefront(object):
       data = fh.read().decode('ascii')
     lines = data.split('\n')
 
-    currentLines = []
+    currentLines = [] # THIS WILL ADD CURRENT TIMESTAMP NO MATTER HOW OLD METRICS ARE
     for line in lines:
       if line:
         lineList = line.split(' ')
         lineList[2] = str(int(time.time()*1000))
         currentLines.append(' '.join(lineList))
-
     lines = currentLines
     return lines
 
   def run(self):
     lines = self.readFile()
-    lines = lines[:5]
+    logger.info('first metric:\n{}'.format(lines[0]))
+    logger.info('{} metrics to send'.format(len(lines)))
     data = '\n'.join(lines) + '\n'
-    print('DATA LENGTH: {}'.format(len(data)))
-    print('DATA: {}'.format(lines))
     epochRandom = str(int(time.time())) + str(randint(1, 9000))
     s3Response = self.s3Client.put_object(
       Body=data, 
@@ -46,7 +44,6 @@ class Telegraf2Wavefront(object):
       Key='oh_my_goodness',
       ACL="bucket-owner-full-control"
     )
-    pprint(s3Response)
     logger.info('S3 Response: {}'.format(s3Response))
     return None
 
@@ -56,7 +53,6 @@ if __name__ == '__main__':
   try:
     T = Telegraf2Wavefront()
     T.run()
-    print('SENT!')
     msg = 'PROGRAM COMPLETED'
     print(msg)
     logger.info(msg)
@@ -64,4 +60,4 @@ if __name__ == '__main__':
     msg = 'PROGRAM FAILED'
     print(msg)
     logger.critical(msg, exc_info=True)
-  logger.info('Done.')
+  logger.info('Done.\n')
